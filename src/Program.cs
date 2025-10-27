c4021bf62a776499a5eff4b8c792e8441c94c6d3
Gabriel Trindade <contato.trindadegabriel@gmail.com>
2025-10-26 22:34:53 -0300

    commit anterior - esqueci de add .

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,9 @@
+using BugStore.settings; // Adicione este using
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSqlServerDbContext(builder.Configuration);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
